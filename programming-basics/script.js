f155a11a1bde6c1f658c96eba28b2129d4ccb47d
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2025-09-29 14:04:33 -0600

    add fetch example

---
+++ programming-basics/script.js
@@ -0,0 +1,9 @@
+fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+
